beb01d7fc26e197e18bcddd09408ddd7c7113177
aynur13 <aynurmusayeva2007@gmail.com>
2024-10-22 09:00:38 +0000

    fix

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -3,18 +3,17 @@
 #include <time.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 
 {
 	int a;
-
 	for (a = 48; a < 58 ; a++)
 	{
-		putchar(a);
+	putchar(a);
 	}
 	putchar('\n');
 	return (0);
