218f12c0000123cf9115c8684f9105bcba5f66d2
aynur13 <aynurmusayeva2007@gmail.com>
2024-10-22 08:47:15 +0000

    fix

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
     int num;
 
     for (num = 0; num < 10; num++)
     {
-        putchar(num + '0');  
+        putchar(num + '0');
     }
 
     putchar('\n'); 
