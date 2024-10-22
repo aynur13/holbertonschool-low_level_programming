c0e801a33f3721786eabf5803dfa507812e051a2
aynur13 <aynurmusayeva2007@gmail.com>
2024-10-22 08:36:31 +0000

    fix

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -4,16 +4,14 @@
 
 int main(void)
 {
-    char ch;
-
-    for (ch = 'a'; ch <= 'z'; ch++)
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
     {
         if (ch != 'e' && ch != 'q')
         {
             putchar(ch);
         }
     }
-
-    putchar('\n');
-    return 0;
+	putchar('\n');
+	return (0);
 }
