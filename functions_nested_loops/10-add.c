e9fbc743e5e7b985ea5f654407edb3cf9808f891
aynur13 <aynurmusayeva2007@gmail.com>
2024-10-23 08:41:15 +0000

    fix

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include <stdio.h>
 /**
-* add - check lowercase.
-* @a: variable1
-* @b: variable2
-* Return: result of addition
+* add - check the code
+* @a: variable
+* @b: variable
+* Return: result.
 */
 int add(int a, int b)
 {
