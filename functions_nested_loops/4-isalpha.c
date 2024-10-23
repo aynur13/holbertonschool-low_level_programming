7990dc5d85aacbd05b6129c7c2ef5b0336289270
aynur13 <aynurmusayeva2007@gmail.com>
2024-10-23 07:16:18 +0000

    fix

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,10 +4,8 @@
 * @c: variable
 * Return: Always 0 (Success)
 */
-int _isalpha(int c);
-{
-	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 90)) 
-	{
+int _isalpha(int c);{
+	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 90)) {
 		return (1);
 	}
 	
