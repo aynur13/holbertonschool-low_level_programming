af5808338468919e061eef3de3793ea55f2e1f4d
aynur13 <aynurmusayeva2007@gmail.com>
2024-10-23 06:59:12 +0000

    fix

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _isalpha - Entry point
- * @c: variable
- * Return: Always 0 (Success)
- */
+* _isalpha - Entry point
+* @c: variable
+* Return: Always 0 (Success)
+*/
 int _isalpha(int c);
 {
 	if ((c >= 97 && c <= 123) || (c >= 65 && c <= 90))
